af2dcc25d3cbbcd0f1a73f30c967b02a2486a128
SOFY. <92982019+SOFYDEVELOPER@users.noreply.github.com>
2026-02-24 12:57:58 +0500

    Update index.sh

--- index.sh
+++ index.sh
@@ -3,4 +3,3 @@ echo "schedule run guid ${schedule_run_guid}"
 
 curl --location --request POST "https://public.sofy.ai/webagent-service/public/scheduled-runs/${schedule_run_guid}/execute" \
 --header "x-sofy-web-auth-key: ${subscription_key}" \
---data '{}'
